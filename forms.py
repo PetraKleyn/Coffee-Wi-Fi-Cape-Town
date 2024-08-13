c84fbf56eef4da691531bc2e5eb8159e0690e5e8
PetraKleyn <169137565+PetraKleyn@users.noreply.github.com>
2024-08-13 11:39:59 +0200

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,33 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
+
+
+# WTForm for creating a blog post
+class CreatePostForm(FlaskForm):
+    name = StringField("Cafe Name", validators=[DataRequired()])
+    address = StringField("Address", validators=[DataRequired()])
+    hours = StringField("Hours", validators=[DataRequired()])
+    location = StringField("Location", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+# Create a form to register new users
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Sign Me Up!")
+
+
+# Create a form to login existing users
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Let Me In!")
+
+
+# Create a form to add comments
+class CommentForm(FlaskForm):
+    comment_text = CKEditorField("Comment", validators=[DataRequired()])
+    submit = SubmitField("Submit Comment")
